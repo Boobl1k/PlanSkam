eae7a7fb6b1760bd1cc6914c7157756382b276ec
Mansur <ahmatowmansur@gmail.com>
2022-04-15 01:58:12 +0300

    base controller getCurrentUser

--- Planscam/Controllers/PsmControllerBase.cs
+++ Planscam/Controllers/PsmControllerBase.cs
@@ -1,7 +1,9 @@
+using System.Linq.Expressions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Planscam.DataAccess;
 using Planscam.Entities;
+using Planscam.Extensions;
 
 namespace Planscam.Controllers;
 
@@ -11,7 +13,15 @@ public abstract class PsmControllerBase : Controller
     protected readonly UserManager<User> UserManager;
     protected readonly SignInManager<User> SignInManager;
 
-    protected PsmControllerBase(AppDbContext dataContext, UserManager<User> userManager, SignInManager<User> signInManager)
+    /// <summary>
+    /// Инициализируется при вызове метода <see cref="GetCurrentUser"/>
+    /// </summary>
+    protected User? CurrentUser { get; private set; }
+    protected async Task<User> GetCurrentUser(params Expression<Func<User, object>>[] includeExpressions) => 
+        CurrentUser ??= await DataContext.GetCurrentUserAsync(UserManager, User, includeExpressions);
+
+    protected PsmControllerBase(AppDbContext dataContext, UserManager<User> userManager,
+        SignInManager<User> signInManager)
     {
         DataContext = dataContext;
         UserManager = userManager;
