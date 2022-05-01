f3b612e3afe5a04b215187400907437df5d4639a
Mansur <ahmatowmansur@gmail.com>
2022-05-01 18:28:58 +0300

    documentation

--- Planscam.Entities/User.cs
+++ Planscam.Entities/User.cs
@@ -7,12 +7,21 @@ public class User : IdentityUser
 {
     public Picture? Picture { get; set; }
 
+    /// <summary>
+    /// liked playlists
+    /// </summary>
     [Required]
     public List<Playlist>? Playlists { get; set; }
 
+    /// <summary>
+    /// special playlist, contains liked tracks
+    /// </summary>
     [Required]
     public FavouriteTracks? FavouriteTracks { get; set; }
-    
+
+    /// <summary>
+    /// only owned by this user, can be not in <see cref="Playlists"/>
+    /// </summary>
     [Required]
     public OwnedPlaylists? OwnedPlaylists { get; set; }
 }
