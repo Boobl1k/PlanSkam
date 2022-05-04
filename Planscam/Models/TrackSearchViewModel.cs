efe73bc50d465f1c375b651354164267427d0bdd
Mansur <ahmatowmansur@gmail.com>
2022-05-04 23:50:12 +0300

    better validation on search

--- Planscam/Models/TrackSearchViewModel.cs
+++ Planscam/Models/TrackSearchViewModel.cs
@@ -7,6 +7,7 @@ public class TrackSearchViewModel
 {
     [Required] public string Query { get; set; } = null!;
     public bool ByAuthors { get; set; }
+    [Range(1, 100000)]
     public int Page { get; set; }
 
     public Playlist? Result { get; set; }
