f274f6146d979d65d5c7136b2c0197e3a6f81ef5
Mansur <ahmatowmansur@gmail.com>
2022-05-21 20:58:11 +0300

    q

--- Planscam/Controllers/HomeController.cs
+++ Planscam/Controllers/HomeController.cs
@@ -44,6 +44,4 @@ public class HomeController : PsmControllerBase
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error() =>
         View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-
-    
 }
