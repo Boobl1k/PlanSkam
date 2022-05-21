e0d3eeb9755dd3f3394394d0e8dfe46cd472c914
Mansur <ahmatowmansur@gmail.com>
2022-05-21 22:36:14 +0300

    rdtuy,mndmty

--- Planscam/Program.cs
+++ Planscam/Program.cs
@@ -1,4 +1,6 @@
 using System.Reflection;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Planscam.DataAccess;
@@ -8,7 +10,7 @@ using Planscam.FsServices;
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
-services.AddDbContext<AppDbContext>(options=>
+services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlConnection"), action =>
         action.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName));
@@ -23,10 +25,21 @@ services.AddScoped<AuthorsRepo>();
 services.AddCors();
 services.AddControllersWithViews();
 
+services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    }).AddCookie(options => { options.LoginPath = "/account/google-login"; })
+    .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+    {
+        options.ClientId = "842296346308-nn6m0csbh0ldm66o9tei07ae9elcjkr4.apps.googleusercontent.com";
+        options.ClientSecret = "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";
+    });
+
 services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = new PathString("/Auth/Login");
-    options.AccessDeniedPath = new PathString("/Auth/AccessDenied");//TODO
+    options.AccessDeniedPath = new PathString("/Auth/AccessDenied"); //TODO
 });
 
 var app = builder.Build();
