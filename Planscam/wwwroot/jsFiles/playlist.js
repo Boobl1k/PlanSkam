2fe1d2317b10f0d318c240ecdb1521fc9f0aec7b
Дмитрий Баев <81518929+DmitryTXCMN@users.noreply.github.com>
2022-05-21 03:19:53 +0300

    Отображение Библиотеки (любимые треки и имеющиеся плейлисты)
    Вынес рендер списка треков и списка плейлистов в отдельные вью. Кнопка добавить работает синхронно с плеером и загруженной страницей.

--- Planscam/wwwroot/jsFiles/playlist.js
+++ Planscam/wwwroot/jsFiles/playlist.js
@@ -24,3 +24,31 @@ function updateLayoutPlaylists() {
         container.innerHTML = request.response.body.innerHTML;
     })
 }
+
+function addTrackToFavourite(id) {
+    btn = document.getElementById(`trackFavBtn${id}`);
+    if (id == JSON.parse(localStorage.playlist).trackIds[localStorage.nowPlayed])
+    {
+        setFavourite(!btn.classList.contains('fi-sr-heart'));
+    }
+    if (btn.classList.contains('fi-sr-heart'))
+        sendAjax("POST", 'json', `/Tracks/RemoveTrackFromFavourite/${id}`, function () {
+            setBtnFavourite(btn, false);
+        });
+    else
+        sendAjax("POST", 'json', `/Tracks/AddTrackToFavourite/${id}`, function () {
+            setBtnFavourite(btn, true);
+        });
+}
+
+function setBtnFavourite(btn, isLiked) {
+    if (isLiked) {
+        btn.classList.remove('fi-rr-heart');
+        btn.classList.add('fi-sr-heart');
+    }
+    else {
+        btn.classList.remove('fi-sr-heart');
+        btn.classList.add('fi-rr-heart');
+    }
+}
+
