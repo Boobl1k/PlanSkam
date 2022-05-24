37350280b9f75be1b16b67eca8843fbccb13f2e9
Mansur <ahmatowmansur@gmail.com>
2022-05-24 19:21:53 +0300

    profile edit test comments

--- Planscam.MobileApi.Tests/ProfileTests.cs
+++ Planscam.MobileApi.Tests/ProfileTests.cs
@@ -10,7 +10,9 @@ namespace Planscam.MobileApi.Tests;
 
 public class ProfileTests : TestBase
 {
-    public ProfileTests(ITestOutputHelper output) : base(output) { }
+    public ProfileTests(ITestOutputHelper output) : base(output)
+    {
+    }
 
     [Fact]
     public async Task Index()
@@ -25,22 +27,27 @@ public class ProfileTests : TestBase
     {
         const string newName = "rew";
         const string oldName = "testttt";
+        //getting data from index before editing
         var indexResponse = await SimpleTest(new HttpRequestMessage(HttpMethod.Get, "Profile/Index")
             .AddTokenToHeaders(Client, null, oldName));
         dynamic index1Obj = JsonConvert.DeserializeObject(await indexResponse.Content.ReadAsStringAsync());
         string email = index1Obj.email;
         string id = index1Obj.id;
+
+        //changing name to new
         var edit1Request = new HttpRequestMessage(HttpMethod.Post, "Profile/Edit")
             .AddTokenToHeaders(Client, null, oldName);
         edit1Request.Content = JsonContent.Create(new {name = newName, email, id});
         var edit1Response = await SimpleTest(edit1Request);
         dynamic editObj = JsonConvert.DeserializeObject(await edit1Response.Content.ReadAsStringAsync());
-        Assert.Equal(newName, (string)editObj.name);
+        Assert.Equal(newName, (string) editObj.name);
+
+        //restoring prev name
         var edit2Request = new HttpRequestMessage(HttpMethod.Post, "Profile/Edit")
             .AddTokenToHeaders(Client, null, newName);
         edit2Request.Content = JsonContent.Create(new {name = oldName, email, id});
         var edit2Response = await SimpleTest(edit2Request);
         dynamic edit2Obj = JsonConvert.DeserializeObject(await edit2Response.Content.ReadAsStringAsync());
-        Assert.Equal(oldName, (string)edit2Obj.name);
+        Assert.Equal(oldName, (string) edit2Obj.name);
     }
 }
